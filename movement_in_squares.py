d0356489de5c2d7b439ddc59ce9f130553eec0f7
hettysymes <hetty.symes@gmail.com>
2024-12-12 11:15:03 +0000

    Added basic checkerboard squashing pattern

--- movement_in_squares.py
+++ movement_in_squares.py
@@ -14,12 +14,22 @@ class Drawing:
         # Add background
         self.dwg.add(self.dwg.rect(insert=(0, 0), size=(self.width, self.height), fill="white"))
         # Define merge centre x position
+        merge_x = self.width * 3/4
         # Add rectangle stripes
         start = True
-        rect_w = 20
-        for x_pos in range(0, self.width, rect_w):
-            self.add_verticle_stripe(rect_w, 20, x_pos, start)
+        original_rect_w = 30
+        rect_h = 30
+        scale = 1
+        x_pos = 0
+        while x_pos < self.width:
+            rect_w = original_rect_w * scale
+            if rect_w < 1:
+                break
+            self.add_verticle_stripe(rect_w, rect_h, x_pos, start)
+            x_pos += rect_w
             start = not start
+            # Closer to merge x, bigger shrink scaling
+            scale = abs(x_pos - merge_x)/300
 
     # x_pos is left border of line
     def add_verticle_stripe(self, rect_w, rect_h, x_pos, start=True):
@@ -40,7 +50,7 @@ class Drawing:
         cairosvg.svg2png(url=f"{self.out_name}.svg", write_to=f"{self.out_name}.png")
 
 if __name__ == '__main__':
-    drawing = Drawing('out/movement_in_squares', 400, 400)
+    drawing = Drawing('out/movement_in_squares', 360, 360)
     drawing.draw()
     drawing.save()
     print("SVG and PNG files saved.")
