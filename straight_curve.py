4f028c7c38129fda25993b82469a3ae677399fc3
hettysymes <hetty.symes@gmail.com>
2025-01-30 17:52:10 +0000

    Simplified straight curve

--- straight_curve.py
+++ straight_curve.py
@@ -1,35 +1,37 @@
 from Drawing import Drawing
+from Warp import PosWarp, RelWarp
 import numpy as np
 
 class StraightCurve(Drawing):
-        
+
+    def __init__(self, out_name, width, height, 
+                 num_vlines=28,
+                 num_hlines=44,
+                 vline_f=lambda i: -1.3*(i**3) + 2*(i**2) + 0.3*i,
+                 hline_piecewise_f_pts=[(0.024, 0.033), (0.220, 0.018), (0.415, 0.035), (0.707, 0.009), (1, 0.033)]):
+        super().__init__(out_name, width, height)
+        self.num_vlines = num_vlines
+        self.num_hlines = num_hlines
+        self.vline_f = vline_f
+        xs, ys = zip(*hline_piecewise_f_pts)
+        self.hline_f = lambda i: np.interp(i, xs, ys)
+
     def draw(self):
         self.add_bg()
-        # Add rectangle stripes
-        x_fun = lambda x: -1.3*(x**3) + 2*(x**2) + 0.3*x
-        y_fun_points = [(0.024, 0.033), (0.220, 0.018), (0.415, 0.035), (0.707, 0.009), (1, 0.033)]
-        xy_points, yy_points = zip(*y_fun_points)
+        vline_xs = PosWarp(self.vline_f).sample(self.num_vlines)*self.width
+        hline_ys = RelWarp(self.hline_f).sample(self.num_hlines)*self.height
+        for i in range(1, len(vline_xs)):
+            self.draw_column(vline_xs[i-1], vline_xs[i], hline_ys)
 
-        prev_x_boundary = 0
-        indices = np.linspace(0, 1, 28)[1:]
-        height_fun = lambda i: np.interp(i, xy_points, yy_points)
-        for i in indices:
-            next_x_boundary = x_fun(i)*self.width
-            self.draw_column(next_x_boundary-prev_x_boundary, prev_x_boundary, height_fun)
-            prev_x_boundary = next_x_boundary
-
-    # x_pos is left border of line
-    def draw_column(self, rect_w, x_pos, height_fun):
-        indices = np.linspace(0, 1, 44)[1:]
-        y_pos = 0
-        for i in indices:
-            rect_h = height_fun(i)*self.height
+    def draw_column(self, x1, x2, hline_ys):
+        for i in range(1, len(hline_ys)):
+            y1 = hline_ys[i-1]
+            y2 = hline_ys[i]
             self.dwg_add(self.dwg.polygon(
-                            points=[(x_pos, y_pos), (x_pos, y_pos+rect_h), (x_pos+rect_w, y_pos+rect_h)],
+                            points=[(x1, y1), (x1, y2), (x2, y2)],
                             fill='black',
                             stroke='none'
                         ))
-            y_pos += rect_h
 
 if __name__ == '__main__':
     drawing = StraightCurve('out/straight_curve', 374, 424)
