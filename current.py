dbcd162e786fdcde3044806820897f9e7fc063a6
hettysymes <hetty.symes@gmail.com>
2025-02-02 20:22:14 +0000

    Has multiple waves

--- current.py
+++ current.py
@@ -1,5 +1,5 @@
 from Drawing import Drawing
-from utils import cubic_f, SineWave
+from utils import cubic_f, CatmullRomCurve
 from Warp import sample_fun
 import numpy as np
 
@@ -8,23 +8,27 @@ class Current(Drawing):
     def __init__(self, out_name, width, height, 
                  trough_peak_y_f=cubic_f(2.4095, -3.8478, 2.3094, 0.0475)):
         super().__init__(out_name, width, height)
-        self.trough_peak_y_f = trough_peak_y_f
-        self.amplitude = 0.1 * self.width
         self.num_splits = 8
-        self.trough_peak_xs = [0.867096363, 0.993142027, 0.886815647, 0.998873766, 0.88618463, 1, 0.936481951, 0.990591667]
+        self.ys = sample_fun(trough_peak_y_f, self.num_splits) * self.height
+        self.x_diffs = np.array([0.126045664, 0.10632638, 0.112058119, 0.112689136, 0.113977462, 0.063680142, 0.054109716]) * self.width
+        num_waves = 80
+        self.x_starts = np.linspace(-100, self.width, num_waves)
         
     def draw(self):
         self.add_bg()
-        # split sine wave into multiple parts
-        trough_start=True
-        ys = sample_fun(self.trough_peak_y_f, self.num_splits)
-        points = []
-        for i in range(1, len(ys)):
-            wavelength = 2 * (ys[i] - ys[i-1])
-            sine_wave = SineWave(self.amplitude, wavelength)
-            trough_y = ys[i-1] if trough_start else ys[i]
-            trough_x = self.trough_peak_xs[i-1] if trough_start else self.trough_peak_xs[i]
-            points += sine_wave.sample(trough_x, trough_y, self.trough_peak_xs[i-1], self.trough_peak_xs[i])
+
+        for x_start in self.x_starts:
+            xs = [x_start]
+            trough = True
+            for d in self.x_diffs:
+                if trough:
+                    xs.append(xs[-1] + d)
+                else:
+                    xs.append(xs[-1] - d)
+                trough = not trough
+            curve = CatmullRomCurve(list(zip(xs, self.ys)))
+            points = curve.regular_sample()
+            self.dwg_add(self.dwg.polyline(points, fill='none', stroke='black'))
             
 
 if __name__ == '__main__':
