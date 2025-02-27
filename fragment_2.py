f069746f55ca34329358d0d85d118b6ddf3a21c6
hettysymes <hetty.symes@gmail.com>
2025-02-27 19:39:45 +0000

    Can draw basic filled arc shape

--- fragment_2.py
+++ fragment_2.py
@@ -19,17 +19,24 @@ class Fragment2(Drawing):
         start_x = 0.322
         curvature_w = 0.026*self.width
         xs = np.array([start_x + i*xw for i in range(6)]) * self.width
+        beziers = []
         for x in xs:
-            self.curve_pts(ys[0], ys[1], x, curvature_w)
+            beziers.append(self.curve_control_pts(ys[0], ys[1], x, curvature_w))
+        for i in range(0, len(beziers)-1, 2):
+            self.draw_polygon(beziers[i], beziers[i+1])
 
-
-    def curve_pts(self, y1, y2, x1, curvature_w):
-        # Bezier control points
+    def curve_control_pts(self, y1, y2, x1, curvature_w):
         xm = x1 + curvature_w
         xc = 2 * xm - x1
         yc = (y1+y2)/2
-        path_data = f"M {x1},{y1} Q {xc},{yc} {x1},{y2}"
-        self.dwg_add(self.dwg.path(d=path_data, stroke="black", fill="none"))
+        return [(x1, y1), (xc, yc), (x1, y2)]
+
+    def draw_polygon(self, bezier1, bezier2):
+        p0, p1, p2 = bezier1
+        p5, p4, p3 = bezier2
+        path_data = f"M {p0[0]},{p0[1]} Q {p1[0]},{p1[1]} {p2[0]},{p2[1]} L {p3[0]},{p3[1]} Q {p4[0]},{p4[1]} {p5[0]},{p5[1]} Z"
+        self.dwg_add(self.dwg.path(d=path_data, fill="black", stroke="none"))
+
 
 if __name__ == '__main__':
     drawing = Fragment2('out/fragment_2', 1000, 0.97)
