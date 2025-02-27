eb93066f1e7056fd34d4309c00286d7c1eb2adc6
hettysymes <hetty.symes@gmail.com>
2025-02-27 21:39:45 +0000

    Num curves per row is linear

--- fragment_2.py
+++ fragment_2.py
@@ -2,6 +2,7 @@ from Drawing import Drawing
 import random
 from utils import cubic_f
 from Warp import sample_fun
+import numpy as np
 
 class CurveRow:
 
@@ -43,22 +44,24 @@ class Fragment2(Drawing):
         super().__init__(out_name, width, height)
         self.y_fun = y_fun
         self.polygon_w = polygon_w * self.width
+        self.num_rows = 11
         
     def draw(self):
         self.add_bg() 
-        ys = sample_fun(self.y_fun, 11)*self.height
+        ys = sample_fun(self.y_fun, self.num_rows)*self.height
+        mean_ncs = np.linspace(4, 40, self.num_rows)
         row = CurveRow(self, 0.322*self.width, ys[0], ys[1], 5, False, 0.026*self.width)
         row.draw()
         for i in range(2, len(ys)):
-            row = self.gen_row(row, ys[i])
+            row = self.gen_row(row, ys[i], mean_ncs[i])
             row.draw()
 
-    def gen_row(self, prev_row, y2):
-        num_curves = max(4, prev_row.num_curves + random.randint(-3, 3)*2)
+    def gen_row(self, prev_row, y2, nc_mean):
+        num_curves = int(np.random.normal(loc=nc_mean, scale=1.0))
         shift = random.randint(-2, 2)
         flip = random.random() < 0.5
         left = (not prev_row.left) if flip else prev_row.left
-        curvature_w = prev_row.curvature_w + random.uniform(-0.005, 0.005)*self.width
+        curvature_w = random.uniform(0.02, 0.08)*self.width
         return CurveRow(self, prev_row.x1 + shift*self.polygon_w, prev_row.y2, y2, num_curves, left, curvature_w)
 
 
