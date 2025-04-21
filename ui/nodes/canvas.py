3bce84f4475c1a115e064138b82ceabf31107ab1
hettysymes <hetty.symes@gmail.com>
2025-04-21 11:43:58 +0100

    Simplified canvas code

--- ui/nodes/canvas.py
+++ ui/nodes/canvas.py
@@ -22,17 +22,12 @@ CANVAS_NODE_INFO = UnitNodeInfo(
 class CanvasNode(UnitNode):
     UNIT_NODE_INFO = CANVAS_NODE_INFO
 
-    def get_svg_path(self, height, wh_ratio):
-        vis = self.visualise(height, wh_ratio)
-        if vis: return vis
-        # No visualisation, return blank canvas
-        return ElementDrawer(f"tmp/{str(self.node_id)}", height, wh_ratio, (Element(), self.prop_vals['bg_col'])).save()
-
     def compute(self):
         return self.input_nodes[0].compute()
 
     def visualise(self, height, wh_ratio):
         element = self.compute()
-        if element:
-            return ElementDrawer(f"tmp/{str(self.node_id)}", height, wh_ratio,
-                                 (element, self.prop_vals['bg_col'])).save()
+        if not element:
+            element = Element()
+        return ElementDrawer(f"tmp/{str(self.node_id)}", height, wh_ratio,
+                             (element, self.prop_vals['bg_col'])).save()
