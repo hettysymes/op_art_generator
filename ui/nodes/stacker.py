333a878365a121eb5f853fe2a49ff29d49992ddb
hettysymes <hetty.symes@gmail.com>
2025-05-01 14:41:18 +0100

    Revert to original stacker

--- ui/nodes/stacker.py
+++ ui/nodes/stacker.py
@@ -1,14 +1,9 @@
-import numpy as np
-
 from ui.nodes.drawers.element_drawer import ElementDrawer
-from ui.nodes.function import PiecewiseFunNode
 from ui.nodes.grid import GridNode
 from ui.nodes.multi_input_handler import handle_multi_inputs
 from ui.nodes.nodes import UnitNode, UnitNodeInfo, PropTypeList, PropType
 from ui.nodes.shape_datatypes import Element
 from ui.nodes.shape_repeater import ShapeRepeaterNode
-from ui.nodes.warp import RelWarpNode
-from ui.nodes.wrapped_element import WrappedElement
 from ui.port_defs import PortDef, PT_Element, PT_Repeatable
 
 STACKER_NODE_INFO = UnitNodeInfo(
@@ -33,65 +28,45 @@ STACKER_NODE_INFO = UnitNodeInfo(
     description="Stack multiple drawings together, either vertically or horizontally."
 )
 
-class Stack(WrappedElement):
-
-    def __init__(self, stack_elem, stack_length, vertical_layout):
-        self.stack_elem = stack_elem
-        self.stack_length = stack_length
-        self.vertical_layout = vertical_layout
-
-    def element(self):
-        return self.stack_elem
-
 
 class StackerNode(UnitNode):
     UNIT_NODE_INFO = STACKER_NODE_INFO
 
+    @staticmethod
+    def helper(elements, wh_diff, vertical_layout):
+        n = len(elements)
+        scale_factor = 1 / wh_diff
+        scaled_elements = []
+        for element in elements:
+            if vertical_layout:
+                scaled_elements.append(element.scale(1, scale_factor))
+            else:
+                scaled_elements.append(element.scale(scale_factor, 1))
+        if vertical_layout:
+            grid = GridNode.helper(None, None, 1, n)
+        else:
+            grid = GridNode.helper(None, None, n, 1)
+        repeated_elem = ShapeRepeaterNode.helper(
+            grid,
+            scaled_elements
+        )
+        final_scale_factor = n / (n - 1 + scale_factor)
+        if vertical_layout:
+            return repeated_elem.scale(1, final_scale_factor)
+        else:
+            return repeated_elem.scale(final_scale_factor, 1)
+
     def compute(self):
         handle_multi_inputs(self.get_input_node('repeatables'), self.prop_vals['elem_order'])
-        vertical_layout = self.get_prop_val('stack_layout') == 'Vertical'
         elements = []
-        sizes = []
         for elem_ref in self.get_prop_val('elem_order'):
             ret_elements = elem_ref.compute()
             if isinstance(ret_elements, Element):
                 ret_elements = [ret_elements]
-            # Add special case for stack input
-            for ret_elem in ret_elements:
-                if isinstance(ret_elem, Stack) and ret_elem.vertical_layout == vertical_layout:
-                    sizes.append(ret_elem.stack_length)
-                    elements.append(ret_elem.stack_elem)
-                else:
-                    sizes.append(1)
-                    elements.append(ret_elem)
+            elements += ret_elements
         if elements:
-            grid_cell_n = len(elements)
-            total_size = sum(sizes)
-            scale_factor = 1 /self.get_prop_val('wh_diff')
-            scaled_elements = []
-            for element in elements:
-                if vertical_layout:
-                    scaled_elements.append(element.scale(1, scale_factor))
-                else:
-                    scaled_elements.append(element.scale(scale_factor, 1))
-            xs = np.linspace(0, 1, grid_cell_n+1)
-            ys = [0] + sizes
-            piecewise_f = PiecewiseFunNode.helper(xs, ys)
-            rel_warp = RelWarpNode.helper(piecewise_f)
-            if vertical_layout:
-                grid = GridNode.helper(None, rel_warp, 1, grid_cell_n)
-            else:
-                grid = GridNode.helper(rel_warp, None, grid_cell_n, 1)
-            repeated_elem = ShapeRepeaterNode.helper(
-                grid,
-                scaled_elements
-            )
-            final_scale_factor = total_size / (total_size - 1 + scale_factor)
-            if vertical_layout:
-                final_scaled_elem = repeated_elem.scale(1, final_scale_factor)
-            else:
-                final_scaled_elem = repeated_elem.scale(final_scale_factor, 1)
-            return Stack(final_scaled_elem, total_size, vertical_layout)
+            return StackerNode.helper(elements, self.get_prop_val('wh_diff'),
+                                      self.get_prop_val('stack_layout') == 'Vertical')
         return None
 
     def visualise(self, temp_dir, height, wh_ratio):
