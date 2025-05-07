71c487667045bb2fa8d5e96fb9629212131a1a2a
hettysymes <hetty.symes@gmail.com>
2025-05-07 13:40:48 +0100

    Colour node has input for colour

--- ui/nodes/colour.py
+++ ui/nodes/colour.py
@@ -1,14 +1,15 @@
 from ui.nodes.nodes import UnitNode, UnitNodeInfo, PropTypeList, PropType
 from ui.nodes.shape import RectangleNode
 from ui.nodes.shape_datatypes import Group
-from ui.port_defs import PortDef, PT_Colour
+from ui.port_defs import PortDef, PT_Colour, PT_Fill
 
 COLOUR_NODE_INFO = UnitNodeInfo(
     name="Colour",
+    prop_port_defs=[PortDef('Colour', PT_Fill, key_name='colour')],
     out_port_defs=[PortDef("Colour", PT_Colour)],
     prop_type_list=PropTypeList([
         PropType("colour", "colour", default_value=(0, 0, 0, 255),
-                 description="Output colour.", display_name="Colour")
+                 description="Output colour.", display_name="Colour", port_modifiable=True)
     ]),
     description="Outputs a desired colour."
 )
