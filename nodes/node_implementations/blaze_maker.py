42be6fc99f7e750e880034336dfb79758e9ae977
hettysymes <hetty.symes@gmail.com>
2025-05-28 18:08:51 +0100

    Added Blaze Maker dialog

--- nodes/node_implementations/blaze_maker.py
+++ nodes/node_implementations/blaze_maker.py
@@ -1,33 +1,77 @@
-# from ui.nodes.node_defs import NodeInfo
-# from ui.nodes.node_implementations.ellipse_sampler import EllipseSamplerNode
-# from ui.nodes.nodes import UnitNode
-# from ui.nodes.prop_defs import PortIO, PortDef, PT_Element, PT_Ellipse, PT_List, PT_Fill, PT_Float, PT_Int, PropEntry
-# from ui.nodes.shape_datatypes import Group, Polygon
-# from ui.nodes.utils import process_rgb
-#
-# DEF_BLAZE_MAKER_INFO = NodeInfo(
-#     description="Input 2+ circle/ellipse shapes to create an image similar to those in the Blaze series by Bridget Riley.",
-#     port_defs={
-#         (PortIO.INPUT, 'ellipses'): PortDef("Input Ellipses", PT_List(PT_Ellipse())),
-#         (PortIO.OUTPUT, '_main'): PortDef("Drawing", PT_Element())
-#     },
-#     prop_entries={
-#         'num_polygons': PropEntry(PT_Int(min_value=1),
-#                                   display_name="Zig-zag number",
-#                                   description="Number of zig-zags, at most 1.",
-#                                   default_value=36),
-#         'angle_diff': PropEntry(PT_Float(),
-#                                 display_name="Zig-zag angle (°)",
-#                                 description="Angle (in degrees) determining the sharpness and direction of the zig-zags. Angles with a higher magnitude give sharper zig-zags. Negative angles give zig-zags in the reverse direction to positive angles.",
-#                                 default_value=20),
-#         'fill': PropEntry(PT_Fill(),
-#                           display_name="Zig-zag colour",
-#                           description="Zig-zag colour.",
-#                           default_value=(0, 0, 0, 255))
-#     }
-# )
-#
-#
+from nodes.node_defs import PrivateNodeInfo, PropDef, PortStatus, ResolvedProps
+from nodes.node_implementations.ellipse_sampler import EllipseSamplerNode
+from nodes.nodes import UnitNode
+from nodes.prop_types import PT_BlazeCircleDef, PT_List, PT_Int, PT_Float, PT_Colour
+from nodes.prop_values import List, Int, Float, Colour
+from nodes.shape_datatypes import Group
+
+DEF_BLAZE_MAKER_INFO = PrivateNodeInfo(
+    description="Create an image similar to those in the Blaze series by Bridget Riley.",
+    prop_defs={
+        'circle_defs': PropDef(
+            prop_type=PT_List(PT_BlazeCircleDef()),
+            display_name="Circle definitions",
+            description="Circle definitions in the Blaze image, defined by a centre offset and radius.",
+            input_port_status=PortStatus.FORBIDDEN,
+            output_port_status=PortStatus.FORBIDDEN,
+            default_value=List(PT_BlazeCircleDef())
+        ),
+        'num_polygons': PropDef(
+            prop_type=PT_Int(min_value=1),
+            display_name="Zig-zag number",
+            description="Number of zig-zags, at most 1.",
+            default_value=Int(36)
+        ),
+        'angle_diff': PropDef(
+            prop_type=PT_Float(),
+            display_name="Zig-zag angle (°)",
+            description="Angle (in degrees) determining the sharpness and direction of the zig-zags. Angles with a higher magnitude give sharper zig-zags. Negative angles give zig-zags in the reverse direction to positive angles.",
+            default_value=Float(20)
+        ),
+        'colour': PropDef(
+            prop_type=PT_Colour(),
+            display_name="Zig-zag colour",
+            description="Zig-zag colour.",
+            default_value=Colour(0, 0, 0, 255)
+        ),
+        '_main': PropDef(
+            input_port_status=PortStatus.FORBIDDEN,
+            output_port_status=PortStatus.COMPULSORY,
+            display_name="Drawing",
+            display_in_props=False
+        )
+    }
+)
+
+class BlazeMakerNode(UnitNode):
+    NAME = "Blaze Maker"
+    DEFAULT_NODE_INFO = DEF_BLAZE_MAKER_INFO
+
+
+    def compute(self, props: ResolvedProps, *args):
+        circle_defs: List[PT_BlazeCircleDef] = props.get('circle_defs')
+        num_samples: float = props.get('num_polygons') * 2
+        ret_group = Group(debug_info="Blaze Maker")
+
+        start_angle = 0
+        samples_list = []
+        for ellipse in ellipses:
+            start_angle += angle_diff
+            angle_diff *= -1
+            samples_list.append(EllipseSamplerNode.helper(ellipse, start_angle, num_samples))
+        # Obtain lines
+        lines = [[] for _ in range(num_samples)]
+        for samples in samples_list:
+            for i, sample in enumerate(samples):
+                lines[i].append(sample)
+        # Draw polygons
+        fill, fill_opacity = process_rgb(colour)
+        for i in range(0, len(lines), 2):
+            points = lines[i - 1] + list(reversed(lines[i]))
+            ret_group.add(Polygon(points, fill, fill_opacity))
+        return ret_group
+
+
 # class BlazeMakerNode(UnitNode):
 #     NAME = "Blaze Maker"
 #     DEFAULT_NODE_INFO = DEF_BLAZE_MAKER_INFO
