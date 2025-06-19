512f4a88689dffacbf0a425e6e54546377ecbcaa
hettysymes <hetty.symes@gmail.com>
2025-06-19 17:54:08 +0100

    Added similarity report for apotryptophanae

--- evaluation/feature_based_similarity.py
+++ evaluation/feature_based_similarity.py
@@ -23,7 +23,7 @@ def extract_features(img_path):
 
 
 artworks = ['movement_in_squares', 'hesitate', 'straight_curve', 'arrest_2', 'conversation', 'blaze', 'shadow_play',
-            'blaze_study', 'arrest_1', 'rise_2', 'cataract_3', 'breathe']
+            'blaze_study', 'arrest_1', 'rise_2', 'cataract_3', 'breathe', 'apotryptophanae']
 
 
 def get_similarity(artwork1, artwork2):
